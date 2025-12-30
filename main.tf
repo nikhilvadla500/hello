205c9a5c7672cdd966e22352b7ee30b0673bc047
Nikhil <nikhilvadla500@gmail.com>
2025-12-31 00:22:39 +0530

    done

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
 provider "google" {
-  project     = "sacred-highway-479412-t7"
-  region      = "us-central1"
-  credentials = file("key.json")
+  project = "sacred-highway-479412-t7"
+  region  = "us-central1"
 }
 
 
-resource "google_storage_bucket" "my_gcs" {
-    count = 2
-    name = "my-demo-bucket-${count.index}"
-    location = "US"
-    storage_class = "STANDARD"
+resource "random_id" "suffix" {
+  byte_length = 4
+}
 
-    uniform_bucket_level_access = true
+resource "google_storage_bucket" "my_gcs" {
+  count         = 2
+  name          = "my-demo-bucket-${count.index}-${random_id.suffix.hex}"
+  location      = "US"
+  storage_class = "STANDARD"
+
+  uniform_bucket_level_access = true
 }
